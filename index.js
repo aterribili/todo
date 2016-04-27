d7472f446960c9088b8638cabe8f194699cb2142
Developer <tech@elo7.com>
2016-04-27 20:11:15 -0300

    Inicio da implementação do MongoDB

--- index.js
+++ index.js
@@ -3,6 +3,36 @@ var app = express();
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://localhost/todo';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
+
+//Lets load the mongoose module in our program
+var mongoose = require('mongoose');//Lets connect to our database using the DB server URL.
+mongoose.connect(url);
+
+var TodoModel = mongoose.model('Todo', {id: Number, description: String, state: Boolean, deleted: Boolean});
+
 var Todo = function(id, description) {
     this.id = id;
     this.description = description;
@@ -28,21 +58,33 @@ var id = 1;
 app.post('/todo/create', (req, res) => {
     if (!req.query.description) return res.sendStatus(400);
     
-    var todo = new Todo(id, req.query.description);
+    var todo = new TodoModel({id: id, description: req.query.description, state: false, deleted: false});
     id++;
     all.push(todo);
+    //Lets try to print and see it. You will see _id is assigned.
+    console.log(todo);
+
+    //Lets save it
+    todo.save(function (err, userObj) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('saved successfully:', userObj);
+      }
+    });
     res.sendStatus(200);
 });
 
 app.get('/todo/list', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    var todos = all.filter(function(todo){
-        return !todo.deleted;
-    }).map(function(todo) {
-        return JSON.stringify({id: todo.id, description: todo.description, state: todo.state});
+    TodoModel.find({}, function(err, docs) {
+        if (!err){ 
+            res.send(JSON.stringify({result: docs}));
+        } else {
+            res.send('Error :(');
+            throw err;
+        }
     });
-    
-    res.send(JSON.stringify({result: todos}));
 });
 
 app.post('/todo/done', (req, res) => {
